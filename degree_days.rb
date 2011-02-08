a3a40bf05f9bc7480b8836146d61eb1a94e7250d
Bryan Helmkamp <bryan@brynary.com>
2011-02-08 17:12:04 -0500

    Use ivars instead of methods. set x = 100 (was 25)

--- degree_days.rb
+++ degree_days.rb
@@ -10,6 +10,20 @@ class DegreeDays
 
   def initialize(options = {})
     @options = options.symbolize_keys
+
+    @base_temperature = @options[:base_temperature] || 65.0
+    @daily_temperatures = @options[:daily_temperatures]
+
+    @insulation_factor = @options[:insulation_factor]
+    @heating_insulation = @options[:heating_insulation] || @insulation_factor || 3
+    @cooling_insulation = @options[:cooling_insulation] || @insulation_factor || 0
+
+    @threshold = @options[:threshold]
+    @heating_threshold = @options[:heating_threshold] || @threshold || 6
+    @cooling_threshold = @options[:cooling_threshold] || @threshold || 3
+
+    @start_date = @options[:start_date].to_date
+    @end_date = @options[:end_date]
   end
 
   def calculate
@@ -18,7 +32,7 @@ class DegreeDays
     @heating_days = 0
     @cooling_days = 0
 
-    daily_temperatures.each do |day|
+    @daily_temperatures.each do |day|
       heating_today = heating_day(day)
       cooling_today = cooling_day(day)
 
@@ -45,62 +59,22 @@ private
 
   def heating_day(temps)
     heat = temps.map {|temp| heating_degree(temp)}.average
-    (heat > heating_threshold) ? heat : false
+    (heat > @heating_threshold) ? heat : false
   end
 
   def cooling_day(temps)
     cool = temps.map {|temp| cooling_degree(temp)}.average
-    (cool > cooling_threshold) ? cool : false
+    (cool > @cooling_threshold) ? cool : false
   end
 
   def heating_degree(temp)
-    deg = base_temperature - (temp + heating_insulation)
+    deg = @base_temperature - (temp + @heating_insulation)
     [deg, 0].max
   end
 
   def cooling_degree(temp)
-    deg = (temp - cooling_insulation) - base_temperature
+    deg = (temp - @cooling_insulation) - @base_temperature
     [deg, 0].max
-  end
-
-  def base_temperature
-    @options[:base_temperature] || 65.0
-  end
-
-  def daily_temperatures
-    @options[:daily_temperatures]
-  end
-
-  def heating_insulation
-    @options[:heating_insulation] || insulation_factor || 3
-  end
-
-  def cooling_insulation
-    @options[:cooling_insulation] || insulation_factor || 0
-  end
-
-  def insulation_factor
-    @options[:insulation_factor]
-  end
-
-  def heating_threshold
-    @options[:heating_threshold] || threshold || 6
-  end
-
-  def cooling_threshold
-    @options[:cooling_threshold] || threshold || 3
-  end
-
-  def threshold
-    @options[:threshold]
-  end
-
-  def start_date
-    @start_date ||= @options[:start_date].to_date
-  end
-
-  def end_date
-    @options[:end_date]
   end
 
 end
